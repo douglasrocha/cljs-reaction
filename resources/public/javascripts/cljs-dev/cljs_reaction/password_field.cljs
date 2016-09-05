c4786782a721995975f23676313d138bb916d407
Douglas Rocha <douglasbellonrocha@gmail.com>
2016-09-04 21:17:02 -0300

    Fixed password field class

--- resources/public/javascripts/cljs-dev/cljs_reaction/password_field.cljs
+++ resources/public/javascripts/cljs-dev/cljs_reaction/password_field.cljs
@@ -4,10 +4,10 @@
               [cljs.core.async :refer (chan put! <!)]))
 
 (defn password-field [*label* *placeholder*]
-    [:div {:class "r-text-field"}
-          [:label {:class "r-lbl-textfield"}
+    [:div {:class "r-password-field"}
+          [:label {:class "r-lbl-password"}
                   *label*]
-          [:input {:class "r-text-textfield"
+          [:input {:class "r-text-password"
                    :placeholder *placeholder*
                    :type :password}
                   nil]])
